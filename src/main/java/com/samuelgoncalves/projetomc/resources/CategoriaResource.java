a9bb44fa721c7f1be7e486827ff02924a2cff8d2
Samuel Gonçalves <samuelesilva@hotmail.com>
2024-11-03 02:20:53 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/samuelgoncalves/projetomc/resources/CategoriaResource.java
+++ src/main/java/com/samuelgoncalves/projetomc/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.samuelgoncalves.projetomc.resources;
 
+import com.samuelgoncalves.projetomc.domain.Categoria;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "REST está funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
